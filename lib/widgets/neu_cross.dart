20b208ea10f5b26c17bd9f7c9277b459086946a2
Andre Haueisen <andrehaueisen@gmail.com>
2020-01-07 14:24:42 -0300

    Animated board pieces

--- lib/widgets/neu_cross.dart
+++ lib/widgets/neu_cross.dart
@@ -1,11 +1,49 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
-class NeuCross extends StatelessWidget {
-  static const double _crossWidth = 20;
+class NeuCross extends StatefulWidget {
   final double size;
 
   NeuCross({@required this.size}) : assert(size != null);
+
+  @override
+  _NeuCrossState createState() => _NeuCrossState();
+}
+
+class _NeuCrossState extends State<NeuCross> with SingleTickerProviderStateMixin {
+  static const double _crossWidth = 20;
+
+  AnimationController _animationController;
+  Animation neuShadowColorAnimation;
+  Animation blackShadowColorAnimation;
+  Animation decorationColorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+
+    neuShadowColorAnimation = ColorTween(begin: Colors.indigo[900], end: Colors.indigo[800]).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    blackShadowColorAnimation = ColorTween(begin: Colors.transparent, end: Colors.black54).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    decorationColorAnimation = ColorTween(begin: Colors.indigo[900], end: Colors.white).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,50 +52,60 @@ class NeuCross extends StatelessWidget {
         Center(
           child: Transform.rotate(
             angle: -math.pi / 4,
-            child: Container(
-              height: size,
-              width: _crossWidth,
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(color: Colors.black54, offset: Offset(1, 1), blurRadius: 2),
-                  BoxShadow(color: Colors.indigo[800], offset: Offset(0, -1), blurRadius: 2)
-                ],
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.indigo[900],
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.white),
-                  borderRadius: BorderRadius.circular(2),
-                  color: Colors.indigo[900],
-                ),
-              ),
+            child: AnimatedBuilder(
+              animation: _animationController,
+              builder: (_, child) {
+                return Container(
+                  height: widget.size,
+                  width: _crossWidth,
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(color: blackShadowColorAnimation.value, offset: Offset(1, 1), blurRadius: 2),
+                      BoxShadow(color: neuShadowColorAnimation.value, offset: Offset(0, -1), blurRadius: 2)
+                    ],
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.indigo[900],
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: decorationColorAnimation.value),
+                      borderRadius: BorderRadius.circular(2),
+                      color: Colors.indigo[900],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ),
         Center(
           child: Transform.rotate(
             angle: math.pi / 4,
-            child: Container(
-              height: size,
-              width: _crossWidth,
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(color: Colors.black54, offset: Offset(1, 1), blurRadius: 2),
-                  BoxShadow(color: Colors.indigo[800], offset: Offset(-1, -1), blurRadius: 2),
-                ],
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.indigo[900],
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 2, color: Colors.white),
-                  borderRadius: BorderRadius.circular(2),
-                  color: Colors.indigo[900],
-                ),
-              ),
+            child: AnimatedBuilder(
+              animation: _animationController,
+              builder: (_, child) {
+                return Container(
+                  height: widget.size,
+                  width: _crossWidth,
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(color: blackShadowColorAnimation.value, offset: Offset(1, 1), blurRadius: 2),
+                      BoxShadow(color: neuShadowColorAnimation.value, offset: Offset(-1, -1), blurRadius: 2),
+                    ],
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.indigo[900],
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: decorationColorAnimation.value),
+                      borderRadius: BorderRadius.circular(2),
+                      color: Colors.indigo[900],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ),
--- lib/widgets/neu_nought.dart
+++ lib/widgets/neu_nought.dart
@@ -1,32 +1,73 @@
 import 'package:flutter/material.dart';
 
-class NeuNought extends StatelessWidget {
-
+class NeuNought extends StatefulWidget {
   final double size;
 
   NeuNought({@required this.size}) : assert(size != null);
 
   @override
+  _NeuNoughtState createState() => _NeuNoughtState();
+}
+
+class _NeuNoughtState extends State<NeuNought> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation neuShadowColorAnimation;
+  Animation blackShadowColorAnimation;
+  Animation decorationColorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+
+    neuShadowColorAnimation = ColorTween(begin: Colors.indigo[900], end: Colors.indigo[800]).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    blackShadowColorAnimation = ColorTween(begin: Colors.transparent, end: Colors.black54).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    decorationColorAnimation = ColorTween(begin: Colors.indigo[900], end: Colors.white).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.decelerate),
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Align(
-      child: Container(
-        padding: const EdgeInsets.all(12),
-        height: size,
-        width: size,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            boxShadow: [
-              BoxShadow(color: Colors.black54, offset: Offset(2, 2), blurRadius: 4),
-              BoxShadow(color: Colors.indigo[800], offset: Offset(-2, -2), blurRadius: 4)
-            ],
-            color: Colors.indigo[900]),
-        child: Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            border: Border.all(width: 12, color: Colors.white),
-            color: Colors.indigo[900],
-          ),
-        ),
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (_, child) {
+          return Container(
+            padding: const EdgeInsets.all(12),
+            height: widget.size,
+            width: widget.size,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(color: blackShadowColorAnimation.value, offset: Offset(2, 2), blurRadius: 4),
+                  BoxShadow(color: neuShadowColorAnimation.value, offset: Offset(-2, -2), blurRadius: 4)
+                ],
+                color: Colors.indigo[900]),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(width: 12, color: decorationColorAnimation.value),
+                color: Colors.indigo[900],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
